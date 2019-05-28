d1a58d50278a939b790e08a34a63836e24b276ad
vitaliks <vitaly@anagorsky.com>
2019-05-29 01:45:23 +0300

    0.0.1

--- sources/index.js
+++ sources/index.js
@@ -9,4 +9,4 @@ const salt = () => Math.random().toString(36).substring(2, 18)
  *
  */
 
-module.exports = () => ('UUID' + salt() + Date.now().toString(36) + salt()).substr(0, 32).toUpperCase()
+module.exports = () => `UUID${salt()}${Date.now().toString(36)}${salt()}`.substr(0, 32).toUpperCase()
