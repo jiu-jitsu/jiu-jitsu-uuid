fb3ab682fe774371551b7932efda582e8abd23e3
Vitaly Anagorsky <vitaly@anagorsky.com>
2021-03-12 10:01:54 +0300

    0.0.5

--- sources/uuid.js
+++ sources/uuid.js
@@ -15,7 +15,7 @@ function uuid () {
 	const now = Date.now().toString(36)
 	const salt1 = salt()
 	const salt2 = salt()
-	const uuid64 = `UUID${salt1}${now}${salt2}`
+	const uuid64 = `X${salt1}Y${now}Z${salt2}X`
 	const uuid32 = uuid64.substr(0, 32).toUpperCase()
 	return uuid32
 }
